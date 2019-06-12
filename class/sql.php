0951714c9691b8a5fbcd7a6814aad01a240600c5
dhardi <denishardi10@outlook.com>
2019-06-12 13:14:47 -0300

    classe ususario criado

--- class/sql.php
+++ class/sql.php
@@ -9,11 +9,11 @@ class sql extends PDO {
 
 
 	}
-private function setParams($statment, $parameters=array()){
+private function setParams($statement, $parameters=array()){
 
 		foreach ($parameters as $key=>$value){
 
-		$this->setParam($key,$value);
+		$this->setParam($statement, $key,$value);
 
 
 
@@ -24,8 +24,8 @@ private function setParams($statment, $parameters=array()){
 
 }
 
-private function setParam($statment,$key,$value){
-	$statment->bindParam($key,$value);
+private function setParam($statement,$key,$value){
+	$statement->bindParam($key,$value);
 
 }
 
