655f6bdf3ac7b1ca8d8c957ce81f132596651a87
dhardi <denishardi10@outlook.com>
2019-06-19 12:36:10 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -1,13 +1,21 @@
 <?php 
 
 require_once("config.php");
+// carrega um usuario 
+//$root = new Usuario();
+//$root->loadById(3);
+//echo $root;
 
-$root = new Usuario();
-$root->loadById(3);
-echo $root;
+// carrega uma lista de usuarios 
+////$list = usuario::getList();
+//echo json_encode($list);
 
+//carrega uma lista de usuario buscando pelo login
+////$search = Usuario::search("e");
+////echo json_encode($search);
+////carrega um usuario usando login e senha 
 
-
-
-
+$usuario = new Usuario();
+$usuario->login("denis","veloster@123");
+echo $usuario;
  ?>
